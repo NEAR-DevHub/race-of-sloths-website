e25726c5d2bdde0ea9bfe14f5f05ad521e8872c3
Andrew <saychuk.andriy@gmail.com>
2024-06-27 02:40:17 +0300

    update preview card mime type

--- src/app/profile/[login]/page.js
+++ src/app/profile/[login]/page.js
@@ -3,6 +3,22 @@ import Profile from "./Profile";
 const apiUrl = process.env.API_URL;
 const badgeUrl = process.env.BADGE_URL;
 
+export async function generateMetadata({ params, searchParams }, parent) {
+  return {
+    title: `Race of Sloths | ${params.login}`,
+    description: "Supercharge your open source project with Race of Sloths",
+    openGraph: {
+      title: `Race of Sloths | ${params.login}`,
+      description: "Supercharge your open source project with Race of Sloths",
+    },
+    twitter: {
+      card: "summary",
+      title: `Race of Sloths | ${params.login}`,
+      description: "Supercharge your open source project with Race of Sloths",
+    },
+  };
+}
+
 export default function ProfilePage() {
   return <Profile apiUrl={apiUrl} badgeUrl={badgeUrl} />;
 }
