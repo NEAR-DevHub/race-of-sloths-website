4bc31dbe8377fd95e652ed5577dafd2ee3a67f03
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-10-08 15:45:32 +0300

    feat: added latest-news redirect to x post. Added static SVGs for bot messages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/latest-news',
+                destination: 'https://x.com/race_of_sloths/status/1837133171604738165',
+                permanent: false,
+            },
+
+        ]
+    },
+};
 
 export default nextConfig;
