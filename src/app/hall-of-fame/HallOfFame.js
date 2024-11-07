56283f663e864b6983c2f2eb6e59db3b367cec1d
akorchyn <artur.yurii.korchynskyi@gmail.com>
2024-11-07 20:08:52 +0200

    mobile for quests

--- src/app/hall-of-fame/HallOfFame.js
+++ src/app/hall-of-fame/HallOfFame.js
@@ -62,14 +62,54 @@ function Line() {
     return <hr className="w-full h-0 border-_shadow" />
 }
 
+function Quests() {
+    const quests = [
+        {
+            number: 1,
+            rank: "Bronze",
+            color: "#EA860D"
+        },
+        {
+            number: 2,
+            rank: "Silver",
+            color: "#A6A5C4"
+        },
+        {
+            number: 3,
+            rank: "Gold",
+            color: "#FFD700"
+        },
+        {
+            number: 4,
+            rank: "Platinum",
+            color: "#E5E4E2"
+        },
+        {
+            number: 5,
+            rank: "Rust",
+            color: "#BE621C"
+        },
+    ];
+
+    return (<section className="flex w-full justify-center flex-wrap gap-x-[88px] md:gap-y-9 gap-y-4 ">
+        {quests.map((quest) => (
+            <div key={quest.rank} className="flex w-full md:w-auto px-[30px] py-4 gap-4 bg-_dark_blue rounded-lg border border-[#30363D]">
+                <Image src={`/images/sloth-${quest.rank.toLowerCase()}.svg`} alt={quest.rank} width={80} height={80} />
+                <div className="flex flex-col items-center m-auto">
+                    <p className="text-2xl" style={{ color: quest.color }}>{quest.rank}</p>
+                    <p className="text-lg">{quest.number} quests</p>
+                </div>
+            </div>
+        ))}
+    </section>);
+}
+
 function HowItWorks() {
     return <section className="flex w-full flex-col gap-6">
         <h1 className="text-4xl">How to get into the <span className="text-_yellow">Hall of Fame</span></h1>
         <p>Complete the following quests to gain additional ranks <br /> in the Sloth Hall of Fame:</p>
         <Image className="w-full h-auto" src={"/images/hof-howto1.svg"} height={600} width={600} alt={"How it works"} />
-        <Image className="w-full h-auto" src={"/images/hof-howto2.svg"} height={600} width={600} alt={"How it works"} />
-
-
+        <Quests />
     </section>
 }
 
